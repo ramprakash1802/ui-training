6a20b6f8eb9d4114559142a78e1fd6df277a0191
M S Ram Prakash <msrprakash@tekion.com>
2023-10-31 12:36:10 +0530

    fixes

--- prototypeInheritance.js
+++ prototypeInheritance.js
@@ -1,11 +1,21 @@
-function Animal (type, height, weight) {
+function Animal (name, type, height, weight) {
+    this.name = name;
     this.type = type;
     this.height = height;
     this.weight = weight;
 };
 
-Animal.prototype.newName = 'Dog';
 
-const newPet = new Animal('Shephard', 20, 20);
+Animal.prototype.speak = function(){
+    console.log(this.name + ' barks');
+}
 
-console.log(newPet.newName);
+function Dog (name, breed) {
+    this.name = name;
+    this.breed = breed;
+}
+
+Dog.prototype = Object.create(Animal.prototype);
+
+const myPet = new Dog('JoJo', 'Indie');
+myPet.speak();
