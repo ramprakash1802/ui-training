0d167a2b781ed4d7a7224be26be4625d3347c0d2
Ram Prakash <msrprakash@tekion.com>
2024-03-23 17:45:39 +0530

    typesrcipt day2

--- Typescript Files/public/JS/index.js
+++ Typescript Files/public/JS/index.js
@@ -18,9 +18,26 @@ const functionEg = (arg1, arg2 = '2') => {
     }
     return `${arg2}`;
 };
+const addNums = (...nums) => {
+    return nums.reduce((prev, next) => prev + next);
+};
+console.log(addNums(1, 2, 3, 4));
+const throwErr = () => {
+    throw new Error('error');
+};
+const giveType = (value) => {
+    if (typeof value === 'string') {
+        return 'String';
+    }
+    if (typeof value === 'number') {
+        return 'Number';
+    }
+    return throwErr(); //never type for non-occuring conditions
+};
 let employee = {
     id: 1234,
     name: 'Jason',
+    workTags: [1, '2']
 };
 const kgToLbs = (weight) => {
     if (typeof weight === "string") {
@@ -39,3 +56,94 @@ const textBox = {
 let unit = "mm";
 const newCustomer = () => ({});
 console.log((_a = newCustomer()) === null || _a === void 0 ? void 0 : _a.birthday); //optional chaining
+const addOrConcat = (a, b, c) => {
+    if (c === 'add') {
+        return a + b;
+    }
+    return '' + a + b;
+};
+let sent = addOrConcat(1, 2, 'concat'); // Type assertion
+const titleElement = document.querySelector('h1'); // non-null assertion
+titleElement.style;
+//Classes
+class Coder {
+    constructor(name, lang, id, //cannot be used while inheriting
+    contact = 1234) {
+        this.name = name;
+        this.lang = lang;
+        this.id = id;
+        this.contact = contact;
+        this.getNumber = () => console.log(this.contact);
+        this.name = name,
+            this.lang = lang,
+            this.id = id;
+        this.contact = contact;
+    }
+}
+class WebDev extends Coder {
+    constructor(computer, name, lang, id) {
+        super(name, lang, id);
+        this.computer = computer;
+        this.computer = computer;
+    }
+}
+const newEmployee = new WebDev('mac', 'Retro', 'Typescript', 1);
+newEmployee.getNumber();
+let bg3 = {
+    name: 'BG#',
+    type: 'PG',
+    playerCount: 1,
+};
+class PCGame {
+    constructor(name, type, playerCount) {
+        this.name = name;
+        this.type = type;
+        this.playerCount = playerCount;
+        this.name = name;
+        this.type = type,
+            this.playerCount = playerCount;
+    }
+}
+const transaction = {
+    pizza: 10,
+    coke: 10,
+};
+for (let i in transaction) {
+    console.log(transaction[i]); // assertion using keyof without using index signature
+}
+transaction.pizza = 1;
+console.log(transaction);
+const echo = (arg) => arg; // Generic Type
+const users = [
+    {
+        id: 1,
+        name: 'Retro',
+    },
+    {
+        id: 2,
+        name: 'Ram,'
+    }
+];
+const checkType = (arg1, arg2) => {
+    return arg1.map(user => user[arg2]);
+};
+console.log(checkType(users, 'name'));
+;
+const Retro = {
+    name: 'Retro',
+    class: 'XII',
+    grade: 'B',
+    attendance: 71,
+};
+const updateStudent = (student, recordToUpdate) => {
+    return Object.assign(Object.assign({}, student), recordToUpdate);
+};
+console.log(updateStudent(Retro, { grade: 'A' }));
+const recordStudent = (student) => {
+    console.log(`${student} is recorded`);
+};
+recordStudent(Retro);
+const colorCodes = {
+    'black': '#000',
+    'white': '#fff',
+};
--- Typescript Files/src/index.ts
+++ Typescript Files/src/index.ts
@@ -17,10 +17,30 @@ const functionEg =  (arg1: number, arg2 = '2') : string  => {
     return `${arg2}`;
 }
 
+const addNums = (...nums : number[]) : number => { //Rest paramaters
+  return nums.reduce((prev, next) => prev + next)
+}
+
+console.log(addNums(1,2,3,4));
+
+const throwErr = () => { // never type
+  throw new Error('error');
+}
+
+const giveType = (value: number | string) : string => {
+  if (typeof value === 'string') {
+    return 'String'
+  } 
+  if (typeof value === 'number') {
+    return 'Number'
+  }
+  return throwErr(); //never type for non-occuring conditions
+}
 
 let employee: Employee = {
   id: 1234,
   name: 'Jason',
+  workTags: [1, '2']
 }
 
 
@@ -28,8 +48,19 @@ type Employee = {
   id: number,
   name: string,
   work?: () => void;
+  workTags: (string | number)[]
 
 } //type alias
+
+interface Employee1 {
+  id: number,
+  name: string,
+  work?: () => void;
+  workTags: (string | number)[]
+
+} 
+
+type fnType = (a: number, b: number) => string;
 
 const kgToLbs = (weight: number | string | null) => { //nullable types
   if (typeof weight === "string") {
@@ -70,3 +101,168 @@ type Customer = {
 const newCustomer = () : Customer => ({});
 
 console.log(newCustomer()?.birthday); //optional chaining
+
+
+const addOrConcat = (a: number, b: number, c: 'add' | 'concat') : string | number => {
+  if (c === 'add') {
+    return a + b
+  }
+  return '' + a + b
+}
+
+let sent : string = addOrConcat(1, 2, 'concat') as string; // Type assertion
+
+const titleElement = document.querySelector('h1')!; // non-null assertion
+
+titleElement.style;
+
+
+//Classes
+
+class Coder {
+
+  constructor (
+    public readonly name: string,
+    public lang: string,
+    private readonly id: number, //cannot be used while inheriting
+    protected contact: number = 1234, //can be used while inheriting
+  ) {
+    this.name = name,
+    this.lang = lang,
+    this.id = id
+    this.contact = contact
+  }
+  public getNumber = () => console.log(this.contact);
+}
+
+class WebDev extends Coder {
+  constructor (
+    public computer: string,
+    name: string,
+    lang: string,
+    id: number
+  ) {
+    super(name, lang, id)
+    this.computer = computer
+  }
+}
+
+const newEmployee = new WebDev('mac', 'Retro', 'Typescript', 1);
+
+newEmployee.getNumber();
+
+interface Game {
+  name: string,
+  type: string,
+  playerCount: number,
+}
+
+let bg3 : Game = {
+  name: 'BG#',
+  type: 'PG',
+  playerCount: 1,
+}
+
+
+class PCGame implements Game { //Implementaton of interfaces
+  constructor (
+    public name: string,
+    public type: string,
+    public playerCount: number
+  ) {
+    this.name = name
+    this.type = type,
+    this.playerCount = playerCount
+  }
+}
+
+interface Transaction { // index signature
+  // [index : string]: number
+  pizza: number,
+  coke: number,
+}
+
+const transaction: Transaction = {
+  pizza: 10,
+  coke: 10,
+}
+
+for (let i in transaction) {
+  console.log(transaction[i as keyof Transaction]); // assertion using keyof without using index signature
+}
+
+transaction.pizza = 1
+console.log(transaction);
+
+
+const echo =  <T> (arg: T) : T => arg; // Generic Type
+
+interface TypeCheck <T> {
+  value: T,
+  is: string,
+}
+
+interface key {
+  id: number
+}
+
+const users = [
+  {
+    id:1,
+    name: 'Retro',
+  },
+  {
+    id:2,
+    name: 'Ram,'
+  }
+]
+
+const checkType = <T extends key, K extends keyof T> (arg1: T[], arg2: K) : T[K][] => { //Multiple generics and extensions
+  return arg1.map(user => user[arg2])
+}
+
+console.log(checkType(users, 'name'));
+
+interface Student {
+  name: string,
+  class: string,
+  grade: string,
+  attendance: number,
+};
+
+const Retro : Student = {
+  name: 'Retro',
+  class: 'XII',
+  grade: 'B',
+  attendance: 71,
+}
+
+const updateStudent = (student: Student, recordToUpdate: Partial<Student>) : Student => { //Partials: for getting a type that is part of an existing type or interface
+  return {
+    ...student,
+    ...recordToUpdate,
+  }
+}
+
+console.log(updateStudent(Retro, { grade: 'A' }));
+
+const recordStudent = (student: Required<Student>) : void => { //All keys are required
+  console.log(`${student} is recorded`);
+}
+
+recordStudent(Retro);
+
+const colorCodes : Record <string, string> = { // combined key value type pairs
+  'black': '#000',
+  'white': '#fff',
+}
+
+type ReportCard = Pick <Student, 'name' | 'grade'> //Picks specified keys in an interface into a new type
+type AttendanceCard = Omit <Student, 'class' | 'grade'> // Omits specified keys
+
+type millimeter = Exclude <Quantity ,'cm'> // Excludes specified key in a union type
+type centimeter = Extract <Quantity, 'cm'> // Extracts specifid key from a union type
+
+type name = 'Ram' | 'Retro' | null;
+
+type NoNullNames = NonNullable <name>
